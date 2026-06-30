686c7e4da2a0405d61367a08600b38aae45db357
PedroSodreUFES <pedro.malini@edu.ufes.br>
2026-06-30 12:02:23 -0300

    feat: knn com alterações do k-valor

--- 05-algoritmos-supervisionados/9-k-nearest-neighbours-knn/knn-classificator.py
+++ 05-algoritmos-supervisionados/9-k-nearest-neighbours-knn/knn-classificator.py
@@ -1,7 +1,61 @@
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-from sklearn.linear_model import LinearRegression
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.model_selection import train_test_split, learning_curve, LearningCurveDisplay
-import numpy as np
+from sklearn.metrics import f1_score
+from sklearn.model_selection import train_test_split
+
+# 1: Ler dados
+df_frutas = pd.read_csv("./dataset/fruits.csv")
+df_frutas.drop(columns=['A_id'], inplace=True)
+df_frutas['Quality'] = df_frutas['Quality'].map({ 'good': 1, 'bad': 0 })
+
+# 2: Treinar modelo
+X = df_frutas.drop(columns=['Quality'])
+y = df_frutas['Quality']
+
+X_train, X_test, y_train, y_test = train_test_split(
+  X,
+  y,
+  test_size=0.3,
+  random_state=51
+)
+
+scores_train = []
+scores_test = []
+
+for i in range(1, 20, 2):
+    clf = KNeighborsClassifier(n_neighbors=i)
+    clf.fit(X=X_train, y=y_train)
+    y_train_pred = clf.predict(X_train)
+    y_test_pred = clf.predict(X_test)
+    f1_score_train = f1_score(y_pred=y_train_pred, y_true=y_train)
+    f1_score_test = f1_score(y_pred=y_test_pred, y_true=y_test)
+    scores_train.append(f1_score_train)
+    scores_test.append(f1_score_test)
+    print(f'{i}: Predição de treino: {f1_score_train} Predição de teste: {f1_score_test}')
+
+df_results = pd.DataFrame({
+    'k': range(1, 20, 2),
+    'train': scores_train,
+    'test': scores_test,
+})
+
+sns.scatterplot(
+    data=df_results,
+    x='k',
+    y='train',
+    label="Treino",
+    color='blue',
+)
+sns.scatterplot(
+    data=df_results,
+    x='k',
+    y='test',
+    label="Teste",
+    color='red',
+)
+plt.ylabel('F1-Score')
+plt.xlabel("K value")
+plt.title("KNN Performance - Mudando K")
+plt.savefig("./dataviz/knn-changing-k.png")
