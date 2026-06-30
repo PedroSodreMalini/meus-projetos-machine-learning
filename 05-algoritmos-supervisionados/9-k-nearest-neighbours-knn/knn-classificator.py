a4c0f45773f2682690e72e3f84e31ac7895bc89e
PedroSodreUFES <pedro.malini@edu.ufes.br>
2026-06-30 11:44:31 -0300

    chore: removing unused imports

---
+++ 05-algoritmos-supervisionados/9-k-nearest-neighbours-knn/knn-classificator.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split, learning_curve, LearningCurveDisplay
+import numpy as np
--- 05-algoritmos-supervisionados/9-k-nearest-neighbours-knn/regressor.py
+++ 05-algoritmos-supervisionados/9-k-nearest-neighbours-knn/regressor.py
@@ -2,9 +2,7 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.model_selection import train_test_split, learning_curve, LearningCurveDisplay
-from sklearn.metrics import f1_score, root_mean_squared_error
+from sklearn.model_selection import learning_curve
 import numpy as np
 
 # 1: Ler dados
